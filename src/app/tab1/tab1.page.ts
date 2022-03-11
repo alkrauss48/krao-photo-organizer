c9db40e8650d264dde178d78f4f6f7ba110dc974
laylark <laylan@gmail.com>
2022-03-10 22:26:54 -0600

    Add SVG and toast

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -6,12 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  letters = 'laylaisawesome';
   counter = 0;
+  activeLetter = '';
 
-  constructor() {}
+  constructor(public toastController: ToastController) {}
 
   buttonClicked() {
-    console.log('clicked');
+    this.activeLetter = this.letters.substring(this.counter,(this.counter + 1));
     this.counter = this.counter + 1;
+    if (this.counter > this.letters.length - 1) {
+        this.counter = 0;
+    }
+  }
+
+  async toastButton() {
+    const toast = await this.toastController.create({
+      message: 'You\'ve been toasted!',
+      duration: 2000
+    });
+    toast.present();
   }
 }
