b7d8331ab35ec28a3276f9c59ecbd48d856186b7
genechao <100798052+genechao@users.noreply.github.com>
2022-03-11 00:25:07 -0600

    Tab 2 image loading/switching sample images

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  photos: string[] = ["assets/bidoof1.jpg", "assets/bidoof2.jpg"];
+  photoActive: number = 0;
+  photoSrc: string = this.photos[this.photoActive];
 
   constructor() {}
 
+  photoClicked() {
+    this.photoActive = (this.photoActive == 0 ? 1 : 0);
+    this.photoSrc = this.photos[this.photoActive];
+  }
 }
